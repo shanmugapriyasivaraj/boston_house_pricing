97ad1632bf0eba393f2dd4e3fbed4d6c70e48a81
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2023-06-13 10:36:46 +0530

    Adding form for home page

--- app.py
+++ app.py
@@ -26,5 +26,13 @@ def predict_api():
     output = linregmodel.predict(new_data)
     return jsonify(output[0])
 
+@app.route('/predict' ,methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output  = linregmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "The House Price Prediction is {}".format(output))
+
 if __name__ == "__main__":
     app.run(debug=True)
